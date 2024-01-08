a3a28ba788fe02c38153770ac5a2169cb8c7dbf0
Jose Vasquez <jvasquez@bst.cl>
2024-01-08 19:16:24 -0300

    get_info_contact_center - parametrizar secret name

--- backend/210_clarovtr_get_info_contact_center/shared/secret_manager.py
+++ backend/210_clarovtr_get_info_contact_center/shared/secret_manager.py
@@ -1,10 +1,10 @@
 import boto3
 import json
- 
- 
+import os
+
+
 def get_value_secret():
-    client = boto3.client('secretsmanager') 
-    credentials = client.get_secret_value(
-        SecretId = "dev/claroVtrNoMolestar"
-    )
-    return  json.loads( credentials['SecretString'] )
+    client = boto3.client("secretsmanager")
+    secret_id = os.environ.get("SECRET_NAME", "dev/claroVtrNoMolestar")
+    credentials = client.get_secret_value(SecretId=secret_id)
+    return json.loads(credentials["SecretString"])
