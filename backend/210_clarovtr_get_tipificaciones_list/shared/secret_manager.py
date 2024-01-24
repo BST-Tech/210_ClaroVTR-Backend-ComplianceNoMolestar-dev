7f118ee537034bb71eeb788a4f6dd2418242f529
Jose Vasquez <jvasquez@bst.cl>
2024-01-24 09:07:06 -0300

    actualizar nombre del secret para distintos ambientes

--- backend/210_clarovtr_get_tipificaciones_list/shared/secret_manager.py
+++ backend/210_clarovtr_get_tipificaciones_list/shared/secret_manager.py
@@ -4,15 +4,18 @@ import os
 
 
 def get_value_secret():
-    dev = False
-    if not os.environ.get("AWS_LAMBDA_FUNCTION_NAME") and not dev:
+    is_deployed = bool(os.environ.get("AWS_LAMBDA_FUNCTION_NAME"))
+    testing_for_prod = True
+    if is_deployed:
+        client = boto3.client("secretsmanager")
+        secret_id = os.environ.get("SECRET_NAME", "prod/claroVtrNoMolestar")
+    elif testing_for_prod:
         session = boto3.Session(profile_name="uw2prod", region_name="us-west-2")
         client = session.client("secretsmanager")
-        secret_id = "dev/claroVtrNoMolestar"
         secret_id = "prod/claroVtrNoMolestar"
     else:
         client = boto3.client("secretsmanager")
-        secret_id = os.environ.get("SECRET_NAME", "dev/claroVtrNoMolestar")
+        secret_id = "dev/claroVtrNoMolestar"
 
     credentials = client.get_secret_value(SecretId=secret_id)
     credentials = json.loads(credentials["SecretString"])
