13c2e9e273a9e1f0a1e15352a7a673b89fba5818
Jose Vasquez <jvasquez@bst.cl>
2024-01-24 15:43:28 -0300

    agregar soporte para insertar tipificacion al ecc dado

--- backend/210_clarovtr_insert_tipificaciones/main.py
+++ backend/210_clarovtr_insert_tipificaciones/main.py
@@ -1,23 +1,22 @@
-import json
 from src.utils import make_data_to_insert
 from src.database import get_id_ct, insert_new_tipificaciones
 
+
 def run(event, context):
-    uid = event['uid']
-    data_to_process = event['data']
-    empresa_ct_id = get_id_ct(uid)[0][0]
+    uid = event["uid"]
+    empresa_ct_id = event.get("empresa_contact_center_id")
+    if not empresa_ct_id:
+        empresa_ct_id = get_id_ct(uid)[0][0]
+    data_to_process = event["data"]
     data_to_insert = make_data_to_insert(data_to_process, empresa_ct_id)
     result_insert = insert_new_tipificaciones(data_to_insert)
     print(f"result_insert {result_insert}")
     if result_insert is None:
         return {
-                'statusCode': 200,
-                'message': "Insertado correctamente",
-                'data_updated': data_to_process
+            "statusCode": 200,
+            "message": "Insertado correctamente",
+            "data_updated": data_to_process,
         }
     else:
         if "Error" in result_insert:
-            return {
-                    'statusCode': 204,
-                    'error': result_insert
-            }
+            return {"statusCode": 204, "error": result_insert}
