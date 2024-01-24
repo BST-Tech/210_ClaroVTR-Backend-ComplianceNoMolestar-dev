c379699d7b42b4d2130bda0a3163fec6fb9d2d46
Jose Vasquez <jvasquez@bst.cl>
2024-01-24 08:32:30 -0300

    update get tipificaciones list

--- backend/210_clarovtr_get_tipificaciones_list/main.py
+++ backend/210_clarovtr_get_tipificaciones_list/main.py
@@ -1,24 +1,45 @@
-import json
 from src.utils import element_to_json
 from src.database import get_tipificaciones_from_api, get_id_empresa_ct
 
+
 def run(event, context):
-    uid = event['uid']
-    contact_center_id = event['data']['contact_center_id']
-    if contact_center_id is None or contact_center_id == "":
-        contact_center_id = get_id_empresa_ct(uid)
-        
-    data_tipificaciones = get_tipificaciones_from_api(uid,contact_center_id)
-    print(data_tipificaciones)
-    
-    if data_tipificaciones is not None:
+    uid = event["uid"]
+    ecc_id = event.get("data", {}).get("empresa_contact_center_id")
+    if not ecc_id:
+        ecc_id = get_id_empresa_ct(uid)
+
+    data_tipificaciones = get_tipificaciones_from_api(ecc_id)
+
+    if data_tipificaciones:
+        json_data = element_to_json(data_tipificaciones)
+        print(f"found {len(json_data)} results")
         return {
-                'statusCode': 200,
-                'tipificaciones':element_to_json(data_tipificaciones)
+            "statusCode": 200,
+            "tipificaciones": json_data,
         }
-    else:
-        return {
-                'statusCode': 204,
-                'tipificaciones':None,
-                'error': 'sin datos que mostrar'
-        }
+    return {
+        "statusCode": 204,
+        "tipificaciones": None,
+        "error": "sin datos que mostrar",
+    }
+
+
+def main():
+    import json
+
+    # from dotenv import load_dotenv
+    # load_dotenv()
+
+    with open("test_cases.json", "r") as f:
+        data = json.load(f)
+
+    test_cases = data["cases"]
+    for test in test_cases:
+        print(test["name"])
+        result = run(test["data"], {})
+        print(result["statusCode"])
+        print("*" * 25)
+
+
+if __name__ == "__main__":
+    main()
--- backend/210_clarovtr_get_tipificaciones_list/shared/secret_manager.py
+++ backend/210_clarovtr_get_tipificaciones_list/shared/secret_manager.py
@@ -1,12 +1,20 @@
 import boto3
 import json
- 
- 
+import os
+
+
 def get_value_secret():
-    #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager.html
-    client = boto3.client('secretsmanager') 
-    credentials = client.get_secret_value(
-        SecretId = "prod/claroVtrNoMolestar"
-    )
-    secretDict = json.loads( credentials['SecretString'] )
-    return  secretDict
+    dev = False
+    if not os.environ.get("AWS_LAMBDA_FUNCTION_NAME") and not dev:
+        session = boto3.Session(profile_name="uw2prod", region_name="us-west-2")
+        client = session.client("secretsmanager")
+        secret_id = "dev/claroVtrNoMolestar"
+        secret_id = "prod/claroVtrNoMolestar"
+    else:
+        client = boto3.client("secretsmanager")
+        secret_id = os.environ.get("SECRET_NAME", "dev/claroVtrNoMolestar")
+
+    credentials = client.get_secret_value(SecretId=secret_id)
+    credentials = json.loads(credentials["SecretString"])
+
+    return credentials
