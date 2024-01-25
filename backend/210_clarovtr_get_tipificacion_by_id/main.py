edb03caeabbe95a315f2e42526d41d8f07cc7970
Jose Vasquez <jvasquez@bst.cl>
2024-01-25 17:24:32 -0300

    update get tipification by id

--- backend/210_clarovtr_get_tipificacion_by_id/main.py
+++ backend/210_clarovtr_get_tipificacion_by_id/main.py
@@ -2,20 +2,20 @@ import json
 from src.utils import element_to_json
 from src.database import get_tipificacion_by_id
 
+
 def run(event, context):
-    uid = event['uid']
-    tipificacion_id = event['id']
+    uid = event["uid"]
+    tipificacion_id = event["id"]
     data_tipificaciones = get_tipificacion_by_id(uid, tipificacion_id)
     print(f"data_tipificaciones {data_tipificaciones}")
-    if len(data_tipificaciones) > 0:
+    if data_tipificaciones and len(data_tipificaciones) > 0:
         return {
-                'statusCode': 200,
-                'tipificaciones':element_to_json(data_tipificaciones)
-            
+            "statusCode": 200,
+            "tipificaciones": element_to_json(data_tipificaciones),
         }
     else:
         return {
-                'statusCode': 204,
-                'tipificaciones':json.dumps('Sin contenido'),
-                'error': 'sin datos que mostrar'
+            "statusCode": 204,
+            "tipificaciones": json.dumps("Sin contenido"),
+            "error": "sin datos que mostrar",
         }
--- backend/210_clarovtr_get_tipificacion_by_id/src/database.py
+++ backend/210_clarovtr_get_tipificacion_by_id/src/database.py
@@ -5,65 +5,73 @@ from shared.secret_manager import get_value_secret
 from shared.cognito import get_user_by_id
 import psycopg2
 
+
 class DatabaseConnection:
-	def connect(self):
-		environ = get_value_secret()
-		try:
-			self.connection = psycopg2.connect(
-                dbname= environ['DB_NAME'],
-                user=environ['DB_USERNAME'],
-                password=environ['DB_PASSWORD'],
-				host=environ['DB_HOST'])
-			return self.connection
-		except Exception as e:
-			print(f"Error al conectar a la base de datos: {e}")
-			return None
+    def connect(self):
+        environ = get_value_secret()
+        try:
+            self.connection = psycopg2.connect(
+                dbname=environ["DB_NAME"],
+                user=environ["DB_USERNAME"],
+                password=environ["DB_PASSWORD"],
+                host=environ["DB_HOST"],
+            )
+            return self.connection
+        except Exception as e:
+            print(f"Error al conectar a la base de datos: {e}")
+            return None
 
-	def execute_many_querys(self, query, params:list=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.executemany(query, params)
-				cursor.close()
-			except Exception as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_many_querys(self, query, params: list = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.executemany(query, params)
+                cursor.close()
+            except Exception as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def execute_query(self, query, params:str=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.execute(query, (params,))
-				result = cursor.fetchall()
-				cursor.close()
-				return result
-			# except Exception as e:
-			except psycopg2.Error as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_query(self, query, params: str = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(query, (params,))
+                result = cursor.fetchall()
+                cursor.close()
+                return result
+            # except Exception as e:
+            except psycopg2.Error as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def close_connection(self):
-		if self.connection is not None:
-			self.connection.commit()
-			self.connection.close()
-			print("Conexión a la base de datos cerrada.")
-		else:
-			print("No hay una conexión activa para cerrar.")
-        
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.commit()
+            self.connection.close()
+            print("Conexión a la base de datos cerrada.")
+        else:
+            print("No hay una conexión activa para cerrar.")
+
+
 def get_tipificacion_by_id(uid, tipificacion_id):
-    email = get_user_by_id(uid)
+    #    email = get_user_by_id(uid)
+    #    query = f"""
+    # select t.id, t.tipificacion, t.nombre_tipificacion, t.contacto, t.venta, ecc.id_contact_center from tipificacion t join empresa_contact_center ecc
+    # on ecc.id = t.id_empresa_ct join perfil_usuario pu
+    # on pu.id_empresa_ct = ecc.id join usuario u
+    # on u.id = pu.id_usuario
+    # where u.email ='{email}' and t.id = {tipificacion_id}"""
+    #    email = get_user_by_id(uid)
     query = f"""
-	select t.id, t.tipificacion, t.nombre_tipificacion, t.contacto, t.venta, ecc.id_contact_center from tipificacion t join empresa_contact_center ecc
-	on ecc.id = t.id_empresa_ct join perfil_usuario pu 
-	on pu.id_empresa_ct = ecc.id join usuario u 
-	on u.id = pu.id_usuario
-	where u.email ='{email}' and t.id = {tipificacion_id}"""
+	select t.id, t.tipificacion, t.nombre_tipificacion, t.contacto, t.venta, t.id_empresa_ct, ecc.id_contact_center from tipificacion t join empresa_contact_center ecc
+	on ecc.id = t.id_empresa_ct
+	where t.id = {tipificacion_id}"""
     try:
         db = DatabaseConnection()
         if db.connect():
