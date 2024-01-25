65b1e528045437a05f5c094267853c0aa823455b
Jose Vasquez <jvasquez@bst.cl>
2024-01-25 17:27:47 -0300

    agregar campo adicional

--- backend/210_clarovtr_get_tipificacion_by_id/src/utils.py
+++ backend/210_clarovtr_get_tipificacion_by_id/src/utils.py
@@ -1,13 +1,15 @@
 def element_to_json(data):
     new_data = []
     for element in data:
-        new_data.append({
-            "id": element[0],
-            "tipificacion": element[1],
-            "nombre_tipificacion": element[2],
-            "contacto": element[3],
-            "venta": element[4],
-            "id_contactCenter": element[5]
-        })
+        new_data.append(
+            {
+                "id": element[0],
+                "tipificacion": element[1],
+                "nombre_tipificacion": element[2],
+                "contacto": element[3],
+                "venta": element[4],
+                "id_contactCenter": element[5],
+                "id_empresa_ct": element[6],
+            }
+        )
     return new_data
-    
