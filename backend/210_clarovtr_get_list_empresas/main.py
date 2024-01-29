9f5e4a53e3cf3a05efe426e7c3b49f2fce2bff0b
Jose Vasquez <jvasquez@bst.cl>
2024-01-29 17:18:08 -0300

    fix format

--- backend/210_clarovtr_get_list_empresas/main.py
+++ backend/210_clarovtr_get_list_empresas/main.py
@@ -2,21 +2,23 @@ import json
 from src.utils import element_to_json
 from src.database import get_users_list
 
+
 def run(event, context):
-    uid = event['uid']
+    uid = event["uid"]
     data_user_list = get_users_list(uid)
-    if len(data_user_list) > 0:
-        return {
-                'statusCode': 200,
-                'data':element_to_json(data_user_list)
-            
-        }
+    if data_user_list and len(data_user_list) > 0:
+        return {"statusCode": 200, "data": element_to_json(data_user_list)}
     else:
         return {
-                'statusCode': 204,
-                'data':json.dumps('Sin contenido'),
-                'error': 'sin datos que mostrar'
+            "statusCode": 204,
+            "data": json.dumps("Sin contenido"),
+            "error": "sin datos que mostrar",
         }
-        
-        
-        
+
+
+def main():
+    run({"uid": ""}, None)
+
+
+if __name__ == "__main__":
+    main()
--- backend/210_clarovtr_get_list_empresas/src/database.py
+++ backend/210_clarovtr_get_list_empresas/src/database.py
@@ -5,60 +5,63 @@ from shared.secret_manager import get_value_secret
 from shared.cognito import get_user_by_id
 import psycopg2
 
+
 class DatabaseConnection:
-	def connect(self):
-		environ = get_value_secret()
-		try:
-			self.connection = psycopg2.connect(
-                dbname= environ['DB_NAME'],
-                user=environ['DB_USERNAME'],
-                password=environ['DB_PASSWORD'],
-				host=environ['DB_HOST'])
-			return self.connection
-		except Exception as e:
-			print(f"Error al conectar a la base de datos: {e}")
-			return None
+    def connect(self):
+        environ = get_value_secret()
+        try:
+            self.connection = psycopg2.connect(
+                dbname=environ["DB_NAME"],
+                user=environ["DB_USERNAME"],
+                password=environ["DB_PASSWORD"],
+                host=environ["DB_HOST"],
+            )
+            return self.connection
+        except Exception as e:
+            print(f"Error al conectar a la base de datos: {e}")
+            return None
 
-	def execute_many_querys(self, query, params:list=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.executemany(query, params)
-				cursor.close()
-			except Exception as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_many_querys(self, query, params: list = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.executemany(query, params)
+                cursor.close()
+            except Exception as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def execute_query(self, query, params:str=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.execute(query, (params,))
-				result = cursor.fetchall()
-				cursor.close()
-				return result
-			# except Exception as e:
-			except psycopg2.Error as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_query(self, query, params: str = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(query, (params,))
+                result = cursor.fetchall()
+                cursor.close()
+                return result
+            # except Exception as e:
+            except psycopg2.Error as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def close_connection(self):
-		if self.connection is not None:
-			self.connection.commit()
-			self.connection.close()
-			print("Conexión a la base de datos cerrada.")
-		else:
-			print("No hay una conexión activa para cerrar.")
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.commit()
+            self.connection.close()
+            print("Conexión a la base de datos cerrada.")
+        else:
+            print("No hay una conexión activa para cerrar.")
+
 
 def get_users_list(uid):
     email = get_user_by_id(uid)
-    query = '''
+    query = """
 	select p.id, u.nombre, u.apellidos, u.email, cc.id, u.activo as estado, p.id_rol ,r.nombre as rol, cc.nombre as nombre_contact_center, e.nombre as nombre_empresa
 		from perfil_usuario p
 		join empresa_contact_center ecc on p.id_empresa_ct = ecc.id
@@ -66,7 +69,7 @@ def get_users_list(uid):
 		join usuario u on p.id_usuario = u.id
 		join rol r on p.id_rol = r.id 
 		join empresa e on e.id = ecc.id_empresa 
-		where u.email = %s'''
+		where u.email = %s"""
     try:
         db = DatabaseConnection()
         if db.connect():
@@ -79,3 +82,4 @@ def get_users_list(uid):
         print(f"Error general: {e}")
     finally:
         db.close_connection()
+
