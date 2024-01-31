42d1b5d4d54639fae15415eafb28704354b4b1b4
Jose Vasquez <jvasquez@bst.cl>
2024-01-31 12:23:21 -0300

    actualizar logica delete tipificacion

--- backend/210_clarovtr_delete_tipificacion/main.py
+++ backend/210_clarovtr_delete_tipificacion/main.py
@@ -1,28 +1,16 @@
-import json
-from src.database import get_empresa_ct, delete_data_tipificaciones
+from src.database import delete_data_tipificaciones
+
 
 def run(event, context):
-    uid = event['uid']
-    data_event = event['data']
+    uid = event["uid"]
+    data_event = event["data"]
     print(f"data_event {data_event}")
-    tipificacion_id = data_event['id']
-    id_contact_center = get_empresa_ct(uid)[0][0]
+    tipificacion_id = data_event["id"]
+    # id_contact_center = get_empresa_ct(uid)[0][0]
     print(tipificacion_id)
-    print(f"id_contact_center {id_contact_center}" )
+    # print(f"id_contact_center {id_contact_center}" )
     # data_updated = compare_data(data_event, data_user_list)
-    
-    
-    if id_contact_center:
-        result_update = delete_data_tipificaciones(tipificacion_id, id_contact_center)
-        print(result_update)
-        return {
-                'statusCode': 200,
-                'message': result_update,
-                'data_updated': data_event #element_to_json(data_user_list)
-            
-        }
-    else:
-        return {
-                'statusCode': 204,
-                'error': 'sin datos que actualizar'
-        }
+
+    result_update = delete_data_tipificaciones(tipificacion_id)
+    print(result_update)
+    return {"statusCode": 200, "message": result_update, "data_updated": data_event}
--- backend/210_clarovtr_delete_tipificacion/src/database.py
+++ backend/210_clarovtr_delete_tipificacion/src/database.py
@@ -8,71 +8,73 @@ from shared.secret_manager import get_value_secret
 from shared.cognito import get_user_by_id
 import psycopg2
 
+
 class DatabaseConnection:
-	def connect(self):
-		environ = get_value_secret()
-		try:
-			self.connection = psycopg2.connect(
-                dbname= environ['DB_NAME'],
-                user=environ['DB_USERNAME'],
-                password=environ['DB_PASSWORD'],
-				host=environ['DB_HOST'])
-			return self.connection
-		except Exception as e:
-			print(f"Error al conectar a la base de datos: {e}")
-			return None
+    def connect(self):
+        environ = get_value_secret()
+        try:
+            self.connection = psycopg2.connect(
+                dbname=environ["DB_NAME"],
+                user=environ["DB_USERNAME"],
+                password=environ["DB_PASSWORD"],
+                host=environ["DB_HOST"],
+            )
+            return self.connection
+        except Exception as e:
+            print(f"Error al conectar a la base de datos: {e}")
+            return None
 
-	def execute_many_querys(self, query, params:list=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.executemany(query, params)
-				cursor.close()
-			except Exception as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_many_querys(self, query, params: list = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.executemany(query, params)
+                cursor.close()
+            except Exception as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def execute_query(self, query, params:str=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.execute(query, (params,))
-				result = cursor.fetchall()
-				cursor.close()
-				return result
-			# except Exception as e:
-			except psycopg2.Error as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_query(self, query, params: str = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(query, (params,))
+                result = cursor.fetchall()
+                cursor.close()
+                return result
+            # except Exception as e:
+            except psycopg2.Error as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def execute_update(self, query, params:str=None):
-		if self.connection is not None:
-			try:
-				cursor = self.connection.cursor()
-				cursor.execute(query, (params,))
-				cursor.close()
-				return "actualizada correctamente"
-			# except Exception as e:
-			except psycopg2.Error as e:
-				print(f"Error al ejecutar la consulta: {e}")
-				return None
-		else:
-			print("No se ha establecido una conexión a la base de datos.")
-			return None
+    def execute_update(self, query, params: str = None):
+        if self.connection is not None:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(query, (params,))
+                cursor.close()
+                return "actualizada correctamente"
+            # except Exception as e:
+            except psycopg2.Error as e:
+                print(f"Error al ejecutar la consulta: {e}")
+                return None
+        else:
+            print("No se ha establecido una conexión a la base de datos.")
+            return None
 
-	def close_connection(self):
-		if self.connection is not None:
-			self.connection.commit()
-			self.connection.close()
-			print("Conexión a la base de datos cerrada.")
-		else:
-			print("No hay una conexión activa para cerrar.")
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.commit()
+            self.connection.close()
+            print("Conexión a la base de datos cerrada.")
+        else:
+            print("No hay una conexión activa para cerrar.")
 
 
 def get_empresa_ct(uid):
@@ -94,11 +96,13 @@ def get_empresa_ct(uid):
         print(f"Error general: {e}")
     finally:
         db.close_connection()
-        
-def delete_data_tipificaciones(id, id_empresa_ct):
-    timestamp = datetime.datetime.fromtimestamp(
-        time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    query = f"UPDATE tipificacion SET activo = 0, deleted_at = '{timestamp}', updated_at = '{timestamp}' WHERE id = {id} and id_empresa_ct = {id_empresa_ct};"
+
+
+def delete_data_tipificaciones(id):
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+    query = f"UPDATE tipificacion SET activo = 0, deleted_at = '{timestamp}', updated_at = '{timestamp}' WHERE id = {id};"
     try:
         db = DatabaseConnection()
         if db.connect():
